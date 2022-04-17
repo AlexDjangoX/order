eb0805551e8a439a34a390d336b036beeb578386
AlexDjangoX <alexmonk17@gmail.com>
2022-04-17 18:51:06 +0200

    FIX: unique ID for each input MealItemForm.js

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,10 +4,13 @@ import Card from "../UI/Card";
 import classes from "../Meals/AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
+// props.key, props.name, props.description, props.price, props.id
+
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => {
     return (
       <MealItem
+        id={meal.id}
         key={meal.id}
         name={meal.name}
         description={meal.description}
@@ -26,3 +29,11 @@ const AvailableMeals = () => {
 };
 
 export default AvailableMeals;
+
+// Last but not least, for that to work, we should also
+// pass id as a prop to MealItem, hence inside of the
+// AvailableMeals component, we should create <MealItem />
+// elements like this:
+
+//     <MealItem
+//         id={meal.id} // this is new!
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -2,6 +2,8 @@ import MealItemForm from "./MealItemForm";
 import React from "react";
 import classes from "./MealItem.module.css";
 
+// props.id
+
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
@@ -12,10 +14,16 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm id={props.id} />
       </div>
     </li>
   );
 };
 
 export default MealItem;
+
+// We just have to make sure that the id props is passed correctly to
+// <MealItemForm /> when that component is being used
+// (i.e. inside of the MealItem component):
+
+//     <MealItemForm id={props.id} />
